fca7073730031d00574ffca0ad2879f9eaf5b2bd
Tan Thanh Nguyen <ngthanhtan91@gmail.com>
2024-08-15 11:04:04 +0800

    Config Google Analytics

--- vendor/integration/utils/configBuilder.ts
+++ vendor/integration/utils/configBuilder.ts
@@ -182,7 +182,7 @@ const getAnalytics = (config: Config) => {
   const _default = {
     vendors: {
       googleAnalytics: {
-        id: undefined,
+        id: 'G-296C7LVQ41',
         partytown: true,
       },
     },
